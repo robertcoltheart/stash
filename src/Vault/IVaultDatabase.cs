e4cac746d98656b4b005a2b081bba34c9d6e1208
Robert Coltheart <robert.coltheart@gmail.com>
2016-02-21 18:27:52 +0800

    Basic API classes

---
+++ src/Vault/IVaultDatabase.cs
@@ -0,0 +1,9 @@
+﻿namespace Vault
+{
+    public interface IVaultDatabase
+    {
+        byte[] Get(byte[] key);
+        void Remove(byte[] key);
+        void Set(byte[] key, byte[] value);
+    }
+}
---
+++ src/Vault/VaultDatabase.cs
@@ -0,0 +1,20 @@
+﻿namespace Vault
+{
+    public class VaultDatabase : IVaultDatabase
+    {
+        public byte[] Get(byte[] key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(byte[] key)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Set(byte[] key, byte[] value)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
