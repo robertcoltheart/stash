314a7cb6ad8e3c08a8818cf1c2e0c251202a7c68
Robert Coltheart <robert.coltheart@gmail.com>
2021-10-11 15:47:32 +0800

    serializer

---
+++ src/Stash/Serialization/BsonSerializer.cs
@@ -0,0 +1,15 @@
+﻿namespace Stash.Serialization
+{
+    public static class BsonSerializer
+    {
+        public static void Serialize<T>(T value, BsonSerializerOptions? options = null)
+        {
+            options ??= BsonSerializerOptions.Default;
+
+            using var buffer = new PooledByteWriterBuffer();
+            using var writer = new BsonWriter(buffer);
+
+            var converter = options.GetConverter(typeof(T));
+        }
+    }
+}
